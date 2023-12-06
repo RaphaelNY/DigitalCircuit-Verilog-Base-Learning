5e578421ec695b6d280e401b3eaa23c94b80c216
RaphaelNY <1278173058@qq.com>
2023-12-06 17:12:57 +0800

    complete simulation codes

---
+++ lab7/lab7.srcs/sources_1/new/8-bit register.v
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/12/06 16:33:04
+// Design Name: 
+// Module Name: 8-bit register
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bit8_register(
+    input CLR,CLK,OE_,
+    input [7:0] D,
+    output [7:0] Q
+    );
+    
+    wire [7:0] S;
+
+    bit_8D bit_8D_inst0(
+        .CLR(CLR),
+        .CLK(CLK),
+        .D(D),
+        .Q(S)
+    );
+
+    TS_8bits_Gate TS_8bits_Gate_inst0(
+        .EN_(OE_),
+        .A(S),
+        .F(Q)
+    );
+
+endmodule
+
+module TS_8bits_Gate(
+    input EN_,
+    input [7:0] A,
+    output reg [7:0] F
+);
+
+    always @* begin
+        if(!EN_) F=A;
+        else F=7'bz;
+    end
+
+endmodule
+
+module bit_8D(
+    input CLR,CLK,
+    input [7:0] D,
+    output reg [7:0] Q
+);
+
+    always @(CLR) begin
+        if(CLR) Q=8'b0;
+    end
+
+    always @(posedge CLK) begin
+        if(!CLR) Q=D;
+    end
+
+endmodule
